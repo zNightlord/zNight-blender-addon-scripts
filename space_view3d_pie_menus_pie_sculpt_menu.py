5b55f1845d31b9ff2e2cde91e2ddac8f451478db
zNight <61040487+zNightlord@users.noreply.github.com>
2020-12-05 20:37:09 +0700

    Modified sculptPie version of 3D Viewport Pie menu
    Modified Sculpt Pie of 3D Viewport Pie Menu arrange and adding new brushes. I know it's UI cluttered.
    also updated shortcut.

--- space_view3d_pie_menus_pie_sculpt_menu.py
+++ space_view3d_pie_menus_pie_sculpt_menu.py
@@ -19,7 +19,7 @@
 # <pep8 compliant>
 
 bl_info = {
-    "name": "Hotkey: 'V'",
+    "name": "Hotkey: 'V' Sculpt Pie",
     "description": "Sculpt Brush Menu",
     "author": "pitiwazou, meta-androcto",
     "version": (0, 1, 0),
